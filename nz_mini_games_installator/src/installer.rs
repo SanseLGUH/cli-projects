1755c65c5a6dc218dea8f20b31e83e386a95a2ec
SanseL <sanselguh@gmail.com>
2025-06-18 06:16:04 +0700

    Add files via upload

--- nz_mini_games_installator/src/installer.rs
+++ nz_mini_games_installator/src/installer.rs
@@ -1,21 +1,19 @@
-use reqwest::{Client, Error};
+use reqwest::{Client, Error as HttpError};
 use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 use std::path::PathBuf;
-use failure::Error as RarError;
 
 const NZGAMES: &str = "https://raw.githubusercontent.com/SanseLGUH/my-cli-scripts/refs/heads/main/nz_mini_games_installator/mini_games/metadata.json";
-const RAWCONTENT: &str = "https://raw.githubusercontent.com/SanseLGUH/my-cli-scripts/refs/heads/main/nz_mini_games_installator/mini_games/AMONGUS";
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MiniGames {
-	path: String,
-	description: String
+	pub path: String,
+	pub description: String
 }
 
 pub type MiniGameCollection = HashMap<String, MiniGames>;
 
-pub async fn games() -> Result<MiniGameCollection, Error> {
+pub async fn games() -> Result<MiniGameCollection, HttpError> {
 	let client = Client::new();
 
 	match client.get(NZGAMES).send().await {
@@ -30,7 +28,7 @@ pub async fn games() -> Result<MiniGameCollection, Error> {
 
 use std::io::Write;
 
-pub async fn install(url: &str, output: &str) -> Result<(), Error> {
+pub async fn install(url: &str, output: &str) -> Result<(), HttpError> {
 	let bytes = reqwest::get(url).await?.bytes().await?.to_vec();
 	let mut f = std::fs::File::create(output).unwrap();
 	f.write_all(&bytes).unwrap();
@@ -38,7 +36,10 @@ pub async fn install(url: &str, output: &str) -> Result<(), Error> {
 	Ok(())
 } 
 
-pub fn unpack(path: &str, output: &str) -> Result<(), RarError> {
-	let archive = rar::Archive::extract_all( "assets/among.rar", output, "" )?;
+pub fn unpack(path: &str, output: &str) -> std::io::Result<()> {
+	let file = std::fs::File::open(path)?;
+	let mut archive = zip::ZipArchive::new(file)?;
+	archive.extract(&std::path::Path::new(output))?;
+
 	Ok(())
 }
--- nz_mini_games_installator/src/main.rs
+++ nz_mini_games_installator/src/main.rs
@@ -5,10 +5,20 @@ mod install_path;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    println!("{:?}", installer::unpack("./among.rar", "./among/"));
+    let minecraft_path = install_path::find().display().to_string();
+    println!("{} <- your minecraft path", minecraft_path);
+
+    println!("\nWant to install DeathRun? [ press enter to install ]");
 
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
+
+    let games = installer::games().await.unwrap();
+
+    if let Some(game) = games.get("AMONGUS") {
+        // installer::install(&game.path, "./nz-mini-game.zip").await;
+        installer::unpack("./AMONGUS.zip", "./");
+    }
 
     Ok(())
 }
